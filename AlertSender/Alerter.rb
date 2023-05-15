782c26836cca98ea7998ef7b7d9b90df6678d4e2
Darren Lasrado <darren.lasrado@gmail.com>
2023-05-15 21:15:59 +0530

    Update logging for AlertSender

--- AlertSender/Alerter.rb
+++ AlertSender/Alerter.rb
@@ -2,10 +2,15 @@ require "kafka"
 require_relative 'Environment.rb'
 
 kafka = Kafka.new(["#{ENV['KAFKA_HOST']}:#{ENV['KAFKA_PORT']}"])
-
-consumer = kafka.consumer(group_id: ENV['GROUP'])
-consumer.subscribe(ENV['TOPIC'])
-consumer.each_message do |message|
-  # TODO: write an emailer here
-  puts "#{message.value}"
+begin
+  puts "Connecting to #{ENV['KAFKA_HOST']}:#{ENV['KAFKA_PORT']}"
+  consumer = kafka.consumer(group_id: ENV['GROUP'])
+  consumer.subscribe(ENV['TOPIC'])
+  consumer.each_message do |message|
+    # TODO: write an emailer here
+    puts "#{message.value}"
+  end
+rescue Exception => e
+  puts "Exception occurred: #{e.message} with trace #{e.backtrace.inspect}"
 end
+
